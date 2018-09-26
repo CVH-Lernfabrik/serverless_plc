b6339ef875623a49477f4a41a1fe4032f2dc5d15
lukasfriedrichsen <friedrichsen.luk@googlemail.com>
2018-09-26 10:11:01 +0200

    deleted redundant stuff

--- AWS_Greengrass/lambdas/opcua_gw/aws-greengrass-core-sdk/index.js
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- */
-exports.Lambda = require('./lambda');
-exports.IotData = require('./iotdata');
--- OPC_UA/model/compileModel.sh
+++
@@ -1,1 +0,0 @@
-python /usr/local/OPC_UA/open62541/tools/nodeset_compiler/nodeset_compiler.py --existing /usr/local/OPC_UA/open62541/deps/ua-nodeset/Schema/Opc.Ua.NodeSet2.xml --xml $1.xml $1
